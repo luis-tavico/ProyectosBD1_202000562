3fc8bd87bc5cb959eca223fa95234a2c47f74c51
luis-tavico <3004359830101@ingenieria.usac.edu.gt>
2024-04-19 21:27:58 -0600

    modified database

--- proyecto2/bd_empresa.sql
+++ proyecto2/bd_empresa.sql
@@ -874,6 +874,26 @@ END //
 
 DELIMITER ;
 
+/* ==================================================CONSULTAR PRODUCTOS Y SERVICIOS================================================== */
+DELIMITER //
+
+CREATE PROCEDURE consultarProductoServicio ()
+BEGIN
+    -- Consultar el listado de productos y servicios
+    SELECT 
+        codigo AS 'Código de producto/servicio',
+        descripcion AS 'Descripcion',
+        CASE
+            WHEN tipo = 1 THEN 'Servicio'
+            WHEN tipo = 2 THEN 'Producto'
+            ELSE 'Desconocido'
+        END AS 'Tipo'
+    FROM 
+        ProductoServicio;
+END //
+
+DELIMITER ;
+
 /* =============================================================FUNCIONES============================================================= */
 /* =================================================================================================================================== */
 /* ==========================================================VALIDAR LETRAS=========================================================== */
@@ -1223,3 +1243,42 @@ BEGIN
     VALUES (NOW(), 'Se ha realizado una acción en la tabla Transaccion.', 'DELETE');
 END//
 DELIMITER ;
+
+/* =====================================================LLAMADAS A PROCEDIMIENTOS===================================================== */
+/* =================================================================================================================================== */
+
+-- registrarTipoCliente
+-- idtipo, nombre, descripcion
+CALL registrarTipoCliente(1, 'Individual Nacional', 'Este tipo de cliente es una persona individual de nacionalidad guatemalteca');
+CALL registrarTipoCliente(2, 'Individual Extranjero', 'Este tipo de cliente es una persona individual de nacionalidad extranjera');
+CALL registrarTipoCliente(3, 'Empresa PyMe', 'Este tipo de cliente es una empresa de tipo pequeña o mediana');
+CALL registrarTipoCliente(4, 'Empresa S.C', 'Este tipo de cliente corresponde a las empresa grandes que tienen una sociedad colectiva');
+
+-- registrarTipoCuenta
+-- idtipo, nombre, descripcion
+CALL registrarTipoCuenta(1,'Cuenta de Cheques','Este tipo de cuenta ofrece la facilidad de emitir cheques para realizar transacciones monetarias');
+CALL registrarTipoCuenta(2,'Cuenta de Ahorros','Esta cuenta genera un interés anual del 2% lo que la hace ideal para guardar fondos a largo plazo');
+CALL registrarTipoCuenta(3,'Cuenta de Ahorro Plus','Con una tasa de interés anual del 10% esta cuenta de ahorros ofrece mayores rendimientos');
+CALL registrarTipoCuenta(4,'Pequeña Cuenta','Una cuenta de ahorros con un interés semestral del 0.5% ideal para pequeños ahorros y movimientos');
+CALL registrarTipoCuenta(5,'Cuenta de Nómina','Diseñada para recibir depósitos de sueldo y realizar pagos con acceso a servicios bancarios básicos');
+CALL registrarTipoCuenta(6,'Cuenta de Inversión','Orientada a inversionistas ofrece opciones de inversión y rendimientos más altos que una cuenta de ahorros estándar');
+
+-- crearProductoServicio
+-- id, tipo, costo, descripcion
+CALL crearProductoServicio(1, 1, 10.00, 'Servicio de tarjeta de debito');
+CALL crearProductoServicio(2, 1, 10.00, 'Servicio de chequera');
+CALL crearProductoServicio(3, 1, 400.00, 'Servicio de asesoramiento financiero');
+CALL crearProductoServicio(4, 1, 5.00, 'Servicio de banca personal');
+CALL crearProductoServicio(5, 1, 30.00, 'Seguro de vida');
+CALL crearProductoServicio(6, 1, 100.00, 'Seguro de vida plus');
+CALL crearProductoServicio(7, 1, 300.00, 'Seguro de automóvil');
+CALL crearProductoServicio(8, 1, 500.00, 'Seguro de automóvil plus');
+CALL crearProductoServicio(9, 1, 00.05, 'Servicio de deposito');
+CALL crearProductoServicio(10, 1, 00.10, 'Servicio de Debito');
+CALL crearProductoServicio(11, 2, 0, 'Pago de energía Eléctrica (EEGSA)');
+CALL crearProductoServicio(12, 2, 0, 'Pago de agua potable (Empagua)');
+CALL crearProductoServicio(13, 2, 0, 'Pago de Matricula USAC');
+CALL crearProductoServicio(14, 2, 0, 'Pago de curso vacaciones USAC');
+CALL crearProductoServicio(15, 2, 0, 'Pago de servicio de internet');
+CALL crearProductoServicio(16, 2, 0, 'Servicio de suscripción plataformas streaming');
+CALL crearProductoServicio(17, 2, 0, 'Servicios Cloud');
